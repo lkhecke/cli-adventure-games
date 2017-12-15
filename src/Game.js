2712d4b319e38784a2411cafbc349a4cb5c9e0dd
lkhecke <31862510+lkhecke@users.noreply.github.com>
2017-12-15 08:07:15 -0600

    Update Game.js

--- src/Game.js
+++ src/Game.js
@@ -8,7 +8,7 @@ function Game() {
   this.inventory = {};
   this.score = 0;
   this.currentRoom = {};
-  this.keyWords = ['help', 'info', 'inventory', 'score', 'save', 'restore', 'throw', 'drop', 'take', 'pick', 'get', 'use', 'move', 'nothing', 'eat', 'inspect'];
+  this.keyWords = ['help', 'info', 'inventory', 'score', 'save', 'restore', 'throw', 'drop', 'take', 'pick', 'get', 'use', 'move', 'nothing'];
 }
 
 Game.prototype.play = function(gsPath) { //Game Script Path
