b017c22e414e47f6de4b04906785209ce88e3436
lkhecke <31862510+lkhecke@users.noreply.github.com>
2017-12-15 16:36:31 -0600

    Update Game.js

--- src/Game.js
+++ src/Game.js
@@ -272,7 +272,7 @@ Game.prototype.offer= function(room, response){
   } else if(room[response]['actions']['offer']) {
       console.log(room[response]['actions']['offer']);
       if(room.alias == 'Desert Dome') {
-        this.pick(room, 'key');
+        self.inventory[item] = 'key';
       }
    } 
 };
