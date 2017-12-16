866e2074cefc4ed00587fd4797f9e33583abc235
lkhecke <31862510+lkhecke@users.noreply.github.com>
2017-12-15 19:27:08 -0600

    Update Game.js

--- src/Game.js
+++ src/Game.js
@@ -247,7 +247,7 @@ Game.prototype.inspect = function(room, response){
     if(room[response]['actions']['inspect']) {
       console.log(room[response]['actions']['inspect']);
     } else {
-      console.log('You cannot inspect ${response}.');
+      console.log('You cannot inspect that item.');
     }
   }
 };
@@ -259,7 +259,7 @@ Game.prototype.talkto= function(room, response){
     if(room[response]['actions']['talk']) {
       console.log(room[response]['actions']['talk']);
     } else {
-      console.log('You cannot talk to ${response}.');
+      console.log('You cannot talk to that person.');
     }
   }
 };
